4929fa88a23e195c268485362897f74a860cb6b0
Jeremy Leggat <jleggat@asu.edu>
2016-02-12 13:08:13 -0700

    Move post meta info.

--- template-parts/content.php
+++ template-parts/content.php
@@ -13,15 +13,12 @@
 	<header class="entry-header">
 		<?php
 			if ( is_single() ) {
-				the_title( '<h1 class="entry-title">', '</h1>' );
+				the_title( '<h2 class="entry-title">', '</h2>' );
 			} else {
 				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 			}
 
 		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php _conversations_posted_on(); ?>
-		</div><!-- .entry-meta -->
 		<?php
 		endif; ?>
 	</header><!-- .entry-header -->
@@ -42,6 +39,9 @@
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
+		<div class="entry-meta">
+			<?php _conversations_posted_on(); ?>
+		</div><!-- .entry-meta -->
 		<?php _conversations_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
